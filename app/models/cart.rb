9b259d7ac65c8708954f82526da67f25d3f3ef0d
Dipayan Deb <chotumotu11@gmail.com>
2017-09-29 12:14:46 +0530

    Fixed bug in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,5 +2,5 @@ class Cart < ApplicationRecord
   belongs_to :user , optional: true
   belongs_to :item , optional: true
 
-  scope :quantity , -> (id_item){ where(item_id: id_item).count }
+  scope :quantity , -> (id_item, id_user){ where("item_id=? and user_id=?","#{id_item}","#{id_user}").count }
 end
